9efaec706901f7c900ea0daf7d367f414ea0d885
ayush <asharma9101@conestogac.on.ca>
2022-11-05 18:42:42 -0400

    Revert ""subtract()" function definition fixed."
    This reverts commit 9c2a60cbb8530982492bb4adea0978ca4ca1e6f0.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,7 +3,6 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-void subtract();
 
 
 void main() {
@@ -15,15 +14,12 @@ void main() {
 	int inputNum;
 
 	printf("Enter operation number: ");
-	scanf_s("%d", &inputNum);
+	scanf_s("%1o", &inputNum);
 
 	switch (inputNum)
 	{
 	case 1:
 		add();
-		break;
-	case 2:
-		subtract();
 	}
 
 }
@@ -48,14 +44,4 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
-}
-
-void subtract() {
-	double num1, num2, result;
-	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
-	result = num1 - num2;
-	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
